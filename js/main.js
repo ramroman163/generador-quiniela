c9122b648a14d6d06d8c3164d01d7cc8f24cf917
ramroman163 <martinezroman@elizalde.edu.ar>
2023-01-31 03:03:06 -0300

    first-commit

---
+++ js/main.js
@@ -0,0 +1,57 @@
+let MAX_NUMBER = 8;
+
+function generateRandomNumbers(numbers) {
+    let result = [];
+
+    // Añadir numeros ya ingresados
+    numbers.forEach(element => {
+        result.push(element);
+    });
+
+    while (result.length < MAX_NUMBER) {
+        let randomNumber = Math.floor(Math.random() * 99);
+        if (!result.includes(randomNumber)) {
+            result.push(randomNumber);
+        }
+    }
+
+    // Ordenar el array
+    result.sort((a,b) => {
+        return a-b;
+    });
+
+    return result;
+}
+
+function showNumbers(arrayNumeros){
+    let stringNumeros = "";
+    let largoArray = arrayNumeros.length;
+
+    for (let i = 0; i < largoArray-1; i++) {
+        stringNumeros += arrayNumeros[i] + "-";
+    }
+
+    stringNumeros += arrayNumeros[largoArray-1];
+
+    let texto = document.getElementById("arrayGenerado");
+    texto.innerHTML = stringNumeros;
+}
+
+function getInputNumers(){
+    let inputs = document.getElementsByClassName("input-numeros");
+    let arrayNumbers = []
+
+    //Tomar valores de los input
+    for (let i = 0; i < inputs.length; i++) {
+        if(inputs[i].value != ''){ arrayNumbers.push(parseInt(inputs[i].value)); }
+    }
+
+    return arrayNumbers;
+}
+
+let button = document.getElementById("generarNumeros");
+button.addEventListener("click", () => {
+    let arrayNumeros = getInputNumers();
+    let numerosAleatorios = generateRandomNumbers(arrayNumeros);
+    showNumbers(numerosAleatorios);
+});
