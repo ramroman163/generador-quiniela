abc77dc13b7eed572a552892601b7cddabe182b8
ramroman163 <martinezroman@elizalde.edu.ar>
2023-02-01 16:04:13 -0300

    second-commit

---
+++ generarCarpetas.bat
@@ -0,0 +1,7 @@
+md html
+md css
+md js
+md img
+md media
+echo.>index.html
+echo.>css\styles.css
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let MAX_NUMBER = 8;
+const MAX_NUMBER = 8;
 
 function generateRandomNumbers(numbers) {
     let result = [];
@@ -16,42 +16,57 @@ function generateRandomNumbers(numbers) {
     }
 
     // Ordenar el array
-    result.sort((a,b) => {
-        return a-b;
+    result.sort((a, b) => {
+        return a - b;
     });
 
     return result;
 }
 
-function showNumbers(arrayNumeros){
+function showNumbers(arrayNumeros) {
     let stringNumeros = "";
     let largoArray = arrayNumeros.length;
 
-    for (let i = 0; i < largoArray-1; i++) {
-        stringNumeros += arrayNumeros[i] + "-";
+    for (let i = 0; i < largoArray - 1; i++) {
+        stringNumeros += arrayNumeros[i].toString().padStart(2, "0") + "-";
     }
 
-    stringNumeros += arrayNumeros[largoArray-1];
+    stringNumeros += arrayNumeros[largoArray - 1];
 
     let texto = document.getElementById("arrayGenerado");
     texto.innerHTML = stringNumeros;
 }
 
-function getInputNumers(){
+function getInputNumers() {
     let inputs = document.getElementsByClassName("input-numeros");
     let arrayNumbers = []
 
     //Tomar valores de los input
     for (let i = 0; i < inputs.length; i++) {
-        if(inputs[i].value != ''){ arrayNumbers.push(parseInt(inputs[i].value)); }
+        if (inputs[i].value != '') {
+            arrayNumbers.push(parseInt(inputs[i].value));
+        }
     }
 
     return arrayNumbers;
 }
 
+let copyButton = document.getElementById("boton-copiar");
 let button = document.getElementById("generarNumeros");
 button.addEventListener("click", () => {
     let arrayNumeros = getInputNumers();
     let numerosAleatorios = generateRandomNumbers(arrayNumeros);
+    copyButton.style.display = "inline-block";
     showNumbers(numerosAleatorios);
 });
+
+copyButton.addEventListener("click", () => {
+    let generatedNumbers = document.getElementById("arrayGenerado");
+    navigator.clipboard.writeText(generatedNumbers.textContent)
+        .then(() => {
+            console.log('Texto copiado al portapapeles');
+        })
+        .catch(err => {
+            console.error('Error al copiar al portapapeles:', err)
+        })
+})
